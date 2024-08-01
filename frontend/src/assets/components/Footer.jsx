b7d3df851fc3eed5d9ec02f21ddd9e0eb1bfa904
DeepakSilaych <deepaksilaych@gmail.com>
2024-08-01 17:43:38 +0530

    rag

--- frontend/src/assets/components/Footer.jsx
+++ frontend/src/assets/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <div className='footer'>
-        Developed by the <Link to="https://itc.gymkhana.iitb.ac.in/web/"> Web Team  | Institute Technical Council </Link>
+        Developed by the <Link to="https://www.linkedin.com/in/deepaksilaych/"> Deepak Silaych | Web Team | Institute Technical Council </Link>
     </div>
   )
 }
